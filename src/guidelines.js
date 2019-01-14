55a3fd5f15944959f6bb50dcac7a4ad9702a7170
Rafael Castro <rccastrorafael@gmail.com>
2019-01-14 11:59:06 -0200

    Update guidelines.js

--- src/guidelines.js
+++ src/guidelines.js
@@ -1,8 +1,8 @@
 exports.createVerticalGuidesLines = (width, gutter, collums) => {
-  const collumn = (width - ((collums + 1) * gutter)) / collums
+  const collum = (width - ((collums + 1) * gutter)) / collums
   const guideLines = Array(collums * 2 + 1)
     .fill(null)
-    .map((x, i) => (i + 1) % 2 === 0 ? collumn : gutter)
+    .map((x, i) => (i + 1) % 2 === 0 ? collum : gutter)
     .reduce((prev, current, index) => {
       prev.push(prev[prev.length - 1] + current)
       return prev
